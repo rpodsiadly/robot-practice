775cd47eb921b3070a86e0318b6c59a39e2f29af
rpodsiadly <105967211+rpodsiadly@users.noreply.github.com>
2022-05-30 11:32:19 +0200

    Update Testdata.py

--- Resources/PageObject/TestData/Testdata.py
+++ Resources/PageObject/TestData/Testdata.py
@@ -1,12 +1,17 @@
 # HomePageData:
+  # Head titles
 hp_head_title = "My Store"
 hp_head_title_women_dir = "Women - My Store"
 hp_head_title_dresses_dir = "Dresses - My Store"
 hp_head_title_tshirts_dir = "T-shirts - My Store"
+  #
 hp_price_of_product_we_looking_for = "$26.00"
+
 # AuthenticationPageData:
+  # Head titles
 ap_head_title = "Login - My Store"
 ap_head_title_auth_positive = "My account - My Store"
+  # Registration test data
 ap_register_mail = "aaaaa22@aaaaa22.com"
 ap_register_mail_too_long = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb@aaaaaaaaaaaaaa.com"
 ap_register_mail_already_taken = "aa@aa.com"
@@ -22,15 +27,19 @@ ap_register_city = "Montgomery"
 ap_register_state = "1"
 ap_register_postcode = "36043"
 ap_lp_register_phonenumber = "733210209"
-ap_lp_login_email_positive = "bob@sparkley.com"
-ap_lp_login_password_positive = "12345"
-ap_lp_login_incorrect_syntax_email = "bobsparkley.com"
-ap_lp_login_email_not_in_database = "bb000b@sparkley.com"
+  # Register failed - alerts
 ap_alert_email_already_exist = "An account using this email address has already been registered. Please enter a valid password or request a new one."
 ap_alert_invalid_date_of_birth = "Invalid date of birth"
 ap_alert_short_password = "passwd is invalid."
 ap_alert_blank_password = "passwd is required."
 ap_alert_email_too_long = "email is too long."
+
+ # Login test data
+ap_lp_login_email_positive = "bob@sparkley.com"
+ap_lp_login_password_positive = "12345"
+ap_lp_login_incorrect_syntax_email = "bobsparkley.com"
+ap_lp_login_email_not_in_database = "bb000b@sparkley.com"
+ # Login failed - alerts
 ap_alert_login_email = "An email address required."
 ap_alert_login_password = "Password is required."
 ap_alert_login_syntax_email = "Invalid email address."
